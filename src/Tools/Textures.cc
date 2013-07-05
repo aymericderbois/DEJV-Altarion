e2d683b5cf2211c788f0a34c19af8970c8556829
Aymeric DERBOIS <azmimik@gmail.com>
2013-07-05 22:31:22 +0200

    Chargement des textures dès le lancement

--- src/Tools/Textures.cc
+++ src/Tools/Textures.cc
@@ -1,11 +1,6 @@
-/* 
- * File:   Textures.cpp
- * Author: Aymeric
- * 
- * Created on 4 juillet 2013, 16:01
- */
-
 #include "Textures.hh"
+#include <dirent.h>
+#include <assert.h>
 
 Textures::Textures() {
 }
@@ -19,5 +14,25 @@ Textures Textures::getInstance() {
 }
 
 void Textures::loadAllTextures() {
-    /* @fixme*/
+    std::string ressourceFolder = "ressources/images/";
+    DIR* dir = NULL;
+    struct dirent* currentFile = NULL;
+    dir = opendir(ressourceFolder.c_str());
+    assert(dir != NULL);
+    
+    while ((currentFile = readdir(dir)) != NULL) {
+        std::string filename(currentFile->d_name);
+        if (filename != "." && filename != "..") {
+            unsigned found = filename.find_last_of(".");
+            std::string filenameWithoutExt = filename.substr(0,found);
+            sf::Texture* texture = new sf::Texture();
+            texture->loadFromFile(ressourceFolder + filename);
+            assert(texture);
+            this->__textures.insert(std::pair<std::string, sf::Texture*>(filenameWithoutExt, texture));
+        }
+    }
+}
+
+sf::Texture* Textures::get(std::string name) {
+    this->__textures.at(name);
 }
